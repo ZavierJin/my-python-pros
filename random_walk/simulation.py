0f4f9673647adf10c5b5d34f958f6900b553e2ec
Zavier <1755274380@qq.com>
2019-08-24 17:45:29 +0800

    terminal position scatter

--- random_walk/simulation.py
+++ random_walk/simulation.py
@@ -1,6 +1,7 @@
 """ simulation """
 from model import Player
-import pylab
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def one_trial(walk_times):
@@ -16,16 +17,22 @@ def one_trial(walk_times):
         # y_list.append(y)
         distances.append(player.get_dist())
 
-    return distances
+    end_x, end_y = player.get_loc()
+
+    return distances, end_x, end_y
 
 
 def simulation(sim_times, walk_times):
     """ many times trial """
     dist_list = []
     aver_dist = []
+    x_list, y_list = [], []
 
     for _ in range(sim_times):
-        dist_list.append(one_trial(walk_times))
+        distances, end_x, end_y = one_trial(walk_times)
+        x_list.append(end_x)
+        y_list.append(end_y)
+        dist_list.append(distances)
 
     # data processing
     for j in range(walk_times):
@@ -34,19 +41,25 @@ def simulation(sim_times, walk_times):
             dist_sum += dist_list[i][j]
         aver_dist.append(dist_sum/sim_times)
 
-    return aver_dist
+    return aver_dist, x_list, y_list
 
 
-def data_visual(aver_dist):
-    """ show the data """
-    pylab.plot(aver_dist)
-    pylab.show()
+def show_ter_pos(x_list, y_list):
+    """ show the terminal position """
+    plt.title("Terminal Position of 500 Random Walks")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    x = np.array(x_list)
+    y = np.array(y_list)
+    color_list = np.sqrt(x**2 + y**2)
+    plt.scatter(x_list, y_list, s=30, c=color_list, marker=(5, 1))
+    plt.show()
 
 
-def average_dist_test():
-    aver_dist = simulation(1000, 500)
-    data_visual(aver_dist)
+def test():
+    aver_dist, x_list, y_list = simulation(100, 500)
+    show_ter_pos(x_list, y_list)
 
 
 if __name__ == '__main__':
-    average_dist_test()
+    test()
